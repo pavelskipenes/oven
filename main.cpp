20a68449b0c562b65b0b83b5644990a756a957b8
Pavel Skipenes <pavelgs@stud.ntnu.no>
2020-12-18 12:28:17 +0100

    init commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @author Pavel Skipenes (pavelgs@stud.ntnu.no)
+ * @brief 
+ * @version 0.1
+ * @date 18-12-2020
+ *
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+void main(){
+    while(true);
+}
