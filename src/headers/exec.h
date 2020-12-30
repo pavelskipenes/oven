5291252abdc84a068b0939808d0ffe82cb702608
Pavel Skipenes <pavelgs@stud.ntnu.no>
2020-12-30 09:50:52 +0100

    Added some formatting rules and formatted source

--- src/headers/exec.h
+++ src/headers/exec.h
@@ -5,16 +5,15 @@
  * @brief Execute host commands with this module
  * @version 0.1
  * @date 23-12-2020
- * 
+ *
  * @copyright Copyright (c) 2020
- * 
+ *
  */
 #include <string>
-
 /**
  * @brief Execute arbitrary command on host
- * 
+ *
  * @param cmd command to execute
  * @return std::string output produced from command.
  */
-std::string exec(const char* cmd);
+std::string exec(const char *cmd);
--- src/oven.cpp
+++ src/oven.cpp
@@ -4,62 +4,59 @@
  * @brief Heat up your cold room in the winter. Or anytime really
  * @version 0.1
  * @date 23-12-2020
- * 
+ *
  * @copyright Copyright (c) 2020
- * 
+ *
  */
-
-#include <chrono>
-#include <thread>
 #include <atomic>
+#include <chrono>
 #include <csignal>
+#include <thread>
 #include <vector>
 
-#include "oven.h"
 #include "core_count.h"
+#include "oven.h"
 
 static std::atomic<bool> terminate_threads(false);
-static void oven_thread(int load_rate, std::atomic<bool>* terminate_ptr);
+static void oven_thread(int load_rate, std::atomic<bool> *terminate_ptr);
 static void signalHandler(int signum);
 
-void oven(int load_rate){
-    signal(SIGINT, signalHandler);
+void oven(int load_rate) {
+	signal(SIGINT, signalHandler);
+	std::vector<std::thread> threads(get_core_count());
 
-    std::vector<std::thread> threads(get_core_count());
+	for (auto &thread : threads) {
+		thread = std::thread(oven_thread, load_rate, &terminate_threads);
+	}
 
-    for(auto &thread : threads){
-        thread = std::thread(oven_thread, load_rate, &terminate_threads);
-    }
-
-    for(auto &thread : threads){
-        thread.join();
-    }
+	for (auto &thread : threads) {
+		thread.join();
+	}
 }
 
 void signalHandler(int signum) {
-    (void)signum; // suppress unused parameter warning
-    terminate_threads = true;
+	(void)signum; // suppress unused parameter warning
+	terminate_threads = true;
 }
 
-void oven_thread(int load_rate, std::atomic<bool>* terminate_ptr){
+void oven_thread(int load_rate, std::atomic<bool> *terminate_ptr) {
+	using namespace std::chrono_literals;
+	const std::chrono::milliseconds CYCLE_TIME = 100ms;
 
-    using namespace std::chrono_literals;
-    const std::chrono::milliseconds CYCLE_TIME = 100ms;
+	while (!terminate_ptr->load()) {
+		auto start = std::chrono::high_resolution_clock::now();
+		auto now = std::chrono::high_resolution_clock::now();
+		auto diff = now - start;
 
-    while(!terminate_ptr->load()){
-        auto start = std::chrono::high_resolution_clock::now();
-        auto now = std::chrono::high_resolution_clock::now();
-        auto diff = now - start;
+		while (true) {
+			now = std::chrono::high_resolution_clock::now();
+			diff = now - start;
 
-        while(true){
-            now = std::chrono::high_resolution_clock::now();
-            diff = now - start;
-            if(diff > (float)load_rate/100 * CYCLE_TIME){
-                break;
-            }
-            // TODO: useless power-consuming non-memory instructions
-        }
-
-        std::this_thread::sleep_for(CYCLE_TIME - diff);
-    }
+			if (diff > (float)load_rate / 100 * CYCLE_TIME) {
+				break;
+			}
+			// TODO: useless power-consuming non-memory instructions
+		}
+		std::this_thread::sleep_for(CYCLE_TIME - diff);
+	}
 }
