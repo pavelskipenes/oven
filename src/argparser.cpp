108bd22d636aa162e65a7bd7559407f946238d17
Pavel Skipenes <pavelgs@stud.ntnu.no>
2020-12-31 05:39:41 +0100

    added static analysis workflow

--- src/argparser.cpp
+++ src/argparser.cpp
@@ -14,9 +14,9 @@
 #include "argparser.h"
 
 bool is_valid_load_rate(char *argv[]) {
-	int load_rate;
 
 	try {
+		int load_rate;
 		load_rate = round(std::stoi(argv[1]));
 		if (load_rate > 101 || load_rate < 0) {
 			throw std::out_of_range("value needs to be between 1 and 100");
